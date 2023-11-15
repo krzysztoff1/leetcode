144f9037a1707795caac8847970627ff16f998be
Krzysztof <halidu13@gmail.com>
2023-11-15 21:22:18 +0100

    feat(cs): add NumJewelsInStones

---
+++ csharp/NumJewelsInStones/Program.cs
@@ -0,0 +1,18 @@
+﻿public class Solution
+{
+    public int NumJewelsInStones(string jewels, string stones)
+    {
+        char[] jewelsArray = jewels.ToCharArray();
+        char[] stonesArray = stones.ToCharArray();
+
+        int output = 0;
+        for (int i = 0; i < stonesArray.Length; i++)
+        {
+            if (jewelsArray.Contains(stonesArray[i]))
+            {
+                output++;
+            }
+        }
+        return output;
+    }
+}
