710f319e4aac1802eadb964c6f3fa76cc23ca6d0
Krzysztof <halidu13@gmail.com>
2023-11-16 08:37:45 +0100

    Add Shuffle, LengthOfLastWord and NumberOfEmployeesWhoMetTarget

--- csharp/NumJewelsInStones/Program.cs
+++ csharp/NumJewelsInStones/Program.cs
@@ -1,18 +1,44 @@
-﻿public class Solution
-{
-    public int NumJewelsInStones(string jewels, string stones)
-    {
-        char[] jewelsArray = jewels.ToCharArray();
-        char[] stonesArray = stones.ToCharArray();
+﻿// input: nums = [2, 5, 1, 3, 4, 7], n = 3
+// output: [2, 3, 5, 4, 1, 7] 
+// explanation: Since x1 = 2, x2 = 5, x3 = 1, y1 = 3, y2 = 4, y3 = 7 then the answer is [2,3,5,4,1,7].
 
-        int output = 0;
-        for (int i = 0; i < stonesArray.Length; i++)
+int[] Shuffle(int[] nums, int n)
+{
+    int[] firstArray = new int[n];
+    int[] secondArray = new int[n];
+
+    for (int i = 0; i < 2 * n; i++)
+    {
+        if (i < n)
         {
-            if (jewelsArray.Contains(stonesArray[i]))
-            {
-                output++;
-            }
+            firstArray[i] = nums[i];
         }
-        return output;
+        else
+        {
+            secondArray[i - n] = nums[i];
+        }
     }
+
+    int[] output = new int[2 * n];
+
+    int j = 0;
+    int k = 0;
+
+    for (int i = 0; i < 2 * n; i++)
+    {
+        if (i % 2 == 0)
+        {
+            output[i] = firstArray[j];
+            j = (j + 1) >= n ? 0 : j + 1;
+        }
+        else
+        {
+            output[i] = secondArray[k];
+            k = (k + 1) >= n ? 0 : k + 1;
+        }
+    }
+
+    return output;
 }
+
+Console.WriteLine($"{string.Join(", ", Shuffle(new int[6] { 2, 5, 1, 3, 4, 7 }, 3))} expected [2,3,5,4,1,7]");
---
+++ csharp/NumberOfEmployeesWhoMetTarget/Program.cs
@@ -0,0 +1,25 @@
+﻿//Input: hours = [0, 1, 2, 3, 4], target = 2
+//Output: 3
+//Explanation: The company wants each employee to work for at least 2 hours.
+//- Employee 0 worked for 0 hours and didn't meet the target.
+//- Employee 1 worked for 1 hours and didn't meet the target.
+//- Employee 2 worked for 2 hours and met the target.
+//- Employee 3 worked for 3 hours and met the target.
+//- Employee 4 worked for 4 hours and met the target.
+//There are 3 employees who met the target.
+
+int NumberOfEmployeesWhoMetTarget(int[] hours, int target)
+{
+    int output = 0;
+    for (int i = 0; i < hours.Length; i++)
+    {
+        if (hours[i] >= target)
+        {
+            output++;
+        }
+    }
+
+    return output;
+}
+
+Console.WriteLine($"{NumberOfEmployeesWhoMetTarget(new int[] { 0, 1, 2, 3, 4 }, 2)}");
