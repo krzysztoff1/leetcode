2b4f57248a00bf04a2853bd7b11a781304dbae36
Krzysztof <halidu13@gmail.com>
2024-01-18 22:00:11 +0100

    Add Palindrome number

---
+++ csharp/palindrome-number/Program.cs
@@ -0,0 +1,22 @@
+﻿bool IsPalindrome(int x) {
+    if (x < 0) {
+        return false;
+    }
+
+    char[] chars = x.ToString().ToCharArray();
+
+    if (string.Join("", chars) == string.Join("", chars.Reverse()))
+    {
+        return true;
+    }
+
+    return false;
+}
+
+Console.WriteLine($"Expected: true, Actual: {IsPalindrome(121)}");
+Console.WriteLine($"Expected: false, Actual: {IsPalindrome(-121)}");
+Console.WriteLine($"Expected: false, Actual: {IsPalindrome(10)}");
+Console.WriteLine($"Expected: false, Actual: {IsPalindrome(-101)}");
+Console.WriteLine($"Expected: true, Actual: {IsPalindrome(0)}");
+Console.WriteLine($"Expected: false, Actual: {IsPalindrome(1000021)}");
+Console.WriteLine($"Expected: true, Actual: {IsPalindrome(12000021)}");
