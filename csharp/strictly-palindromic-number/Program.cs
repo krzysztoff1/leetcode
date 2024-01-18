8496cc527dc8d678a8e9917967d700a87e7ff9b3
Krzysztof <halidu13@gmail.com>
2024-01-18 22:30:48 +0100

    Add Strictly Palindromic Number

---
+++ csharp/strictly-palindromic-number/Program.cs
@@ -0,0 +1,25 @@
+﻿bool IsStrictlyPalindromic(int n)
+{
+    for (int i = 2; i <= n - 2; i++)
+    {
+        string binary = "";
+        int temp = i;
+        while (temp > 0)
+        {
+            binary = (temp % 2) + binary;
+            temp /= 2;
+        }
+
+        char[] chars = binary.ToCharArray();
+
+        if (string.Join("", chars) == string.Join("", chars.Reverse()))
+        {
+            continue;
+        }
+
+        return false;
+    }
+    return true;
+}
+
+Console.WriteLine($"Case: 5, Expected: False, Actual: {IsStrictlyPalindromic(5)}");
