99072a07f08905990a3505bd965bf648abc449e8
Krzysztof <halidu13@gmail.com>
2024-02-17 19:56:21 +0100

    Add Longest common prefix

---
+++ csharp/longest-common-prefix/Program.cs
@@ -0,0 +1,19 @@
+﻿string LongestCommonPrefix(string[] strs)
+{
+    int maxLenOfCommonPrefix = strs.Min(y => y.Length);
+    string shortestWord = strs.FirstOrDefault(x => x.Length == maxLenOfCommonPrefix) ?? "";
+    string output = shortestWord;
+
+
+    while (true)
+    {
+        if (strs.All(s => s.StartsWith(output)))
+        {
+            return output;
+        }
+
+        output = output.Remove(output.Length - 1);
+    }
+}
+
+Console.WriteLine(LongestCommonPrefix(["flower", "flow", "flight"]));
