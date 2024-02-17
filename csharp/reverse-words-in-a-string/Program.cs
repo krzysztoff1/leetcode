ea8d8af9cf8396da98486870657039dab974e455
Krzysztof <halidu13@gmail.com>
2024-02-17 20:33:51 +0100

    Add Reverse Words in a String

---
+++ csharp/reverse-words-in-a-string/Program.cs
@@ -0,0 +1,22 @@
+﻿
+string ReverseWords(string str)
+{
+    string[] arr = str.Trim().Split(" ");
+    List<string> reversed = [];
+
+    for (int i = arr.Length - 1; i >= 0; i--)
+    {
+        if (arr[i].Length == 0)
+        {
+            continue;
+        }
+
+        reversed.Add(arr[i]);
+    }
+
+
+    return string.Join(" ", [.. reversed]);
+}
+
+
+Console.WriteLine(ReverseWords("a good   example"));
