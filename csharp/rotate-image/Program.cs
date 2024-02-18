706771b9bc84c11b23693f5e5045f45f3dcd24d0
Krzysztof <halidu13@gmail.com>
2024-02-18 11:55:52 +0100

    Add Rotate Image

---
+++ csharp/rotate-image/Program.cs
@@ -0,0 +1,39 @@
+﻿void PrintMatrix(int[][] matrix)
+{
+    for (int i = 0; i < matrix.Length; i++)
+    {
+        for (int j = 0; j < matrix.Length; j++)
+        {
+            Console.Write(matrix[i][j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void Rotate(int[][] matrix)
+{
+    Array.Reverse(matrix);
+
+    for (int i = 0; i < matrix.Length; i++)
+    {
+        for (int j = 0; j < matrix.Length; j++)
+        {
+            if (i < j)
+            {
+                (matrix[j][i], matrix[i][j]) = (matrix[i][j], matrix[j][i]);
+            }
+        }
+    }
+}
+
+Console.WriteLine("Input:");
+int[][] matrix =
+[
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
+PrintMatrix(matrix);
+Rotate(matrix);
+Console.WriteLine("Output:");
+PrintMatrix(matrix);
