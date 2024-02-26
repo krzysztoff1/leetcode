ee42d7780b37bf377403d54017ed91baabde9c43
Krzysztof <78202674+krzysztoff1@users.noreply.github.com>
2024-02-26 23:16:34 +0100

    Add Group Anagrams (#24)

---
+++ csharp/group-anagrams/Program.cs
@@ -0,0 +1,46 @@
+﻿
+IList<IList<string>> GroupAnagrams(string[] strs)
+{
+    Dictionary<string, IList<string>> dict = [];
+
+    for (int i = 0; i < strs.Length; i++)
+    {
+        char[] sortedChars = strs[i].ToCharArray();
+        sortedChars = sortedChars.OrderBy(c => c).ToArray();
+        string wordWithSortedChars = new(sortedChars);
+
+        if (dict.ContainsKey(wordWithSortedChars))
+        {
+            dict[wordWithSortedChars].Add(strs[i]);
+
+        }
+        else
+        {
+            dict.Add(wordWithSortedChars, [strs[i]]);
+        }
+
+    }
+
+    return [.. dict.Values];
+}
+
+Console.WriteLine("Input:");
+
+string[] strs = ["hhhhu", "tttti", "tttit", "hhhuh", "hhuhh", "tittt"];
+
+Console.WriteLine(String.Join(", ", strs));
+
+
+IList<IList<string>> output = GroupAnagrams(strs);
+
+Console.WriteLine("Output: ");
+
+foreach (var group in output)
+{
+    Console.WriteLine();
+    foreach (var strings in group)
+    {
+        Console.Write(strings + ", ");
+    }
+    Console.WriteLine();
+}
